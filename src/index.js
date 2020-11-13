cd88ca62c6e790506deeb9c019ed48679bc3e81b
Tizita Hailu <tul43593@temple.edu>
2020-11-13 15:24:40 -0500

    new index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//key - 431d512a3f5465a139bb5e58f03fdaa2
+const fetch = require("node-fetch");
+//const URL = require("url");
+let url = new URL("https://developers.zomato.com/api/v2.1/search")
+let latt = "38.715140",long ="-90.418950",searchKey="burgers";
+let radius = 1000;
+let userKey = "431d512a3f5465a139bb5e58f03fdaa2";
+let header = {
+    "user-key": userKey
+}
 
+let params ={
+    "lon" : long,
+    "lat" : latt,
+    "q" : searchKey
+}
+url.search = new URLSearchParams(params).toString(); 
+fetch(url,{
+    headers:header
+}).then((res)=>{
+    return res.json()
+}).then((json)=>{
+    console.log(json);
+}).catch(err => console.log(err));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//url - https://developers.zomato.com/api/v2.1/search
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
